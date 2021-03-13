e78cff070c9e6d8c78fd717512a89e2d132ee04f
Turumuru <58406669+Turumuru@users.noreply.github.com>
2021-03-14 07:14:30 +1000

    Add files via upload

--- DSP/MainWindow.xaml.cs
+++ DSP/MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.IO;
+using Microsoft.Win32;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
 
 namespace DSP
 {
@@ -23,6 +26,19 @@ namespace DSP
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void loading_file(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовый файл (*.txt)|*.txt|Звуковой файл (*.wav)|*.wav|Dat-файл (*.dat)|*.dat";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string data = File.ReadAllText(openFileDialog.FileName);
+                ChanelsPage сhanelsPage = new ChanelsPage();
+                сhanelsPage.Show();
+                сhanelsPage.txtEditor.Text = data;
+            }
         }
     }
 }
