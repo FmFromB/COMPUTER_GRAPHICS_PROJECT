410991e47c4059fd32c1987609ab2e2e494a832f
FmFromB <63696790+FmFromB@users.noreply.github.com>
2021-04-03 12:21:36 +1000

    Add files via upload

--- DSP/ChannelAnalyzer.xaml.cs
+++ DSP/ChannelAnalyzer.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,7 +10,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using LiveCharts;
-using LiveCharts.Wpf;
 
 namespace DSP
 {
@@ -20,117 +18,33 @@ namespace DSP
     /// </summary>
     public partial class ChannelAnalyzer : Window
     {
-        public int a;
-        public ChannelAnalyzer(int channel_num, int samples_num, string[] date_channel, string channels_names)
+        public ChannelAnalyzer()
         {
             InitializeComponent();
-
-            //Ахуительное решение 10 из 10
-            if (samples_num > 400000)
-            {
-                a = samples_num / 300;
-            }
-            if (samples_num > 212000)
-            {
-                a = samples_num / 1000;
-            }
-            if (samples_num > 104000)
-            {
-                a = samples_num / 419;
-            }
-            if (samples_num > 41000)
-            {
-                a = samples_num / 466;
-            }
-            if (samples_num > 15000)
-            {
-                a = samples_num / 524;
-            }
-            if (samples_num > 1200)
-            {
-                a = samples_num / 404;
-            }
-
-            string[] date_channels = new string[channel_num + 1];
-            for (int i = 0; i < samples_num; i += a)
-            {
-                string[] tmp = date_channel[i].Split(' ');
-                for (int j = 0; j < channel_num; j++)
-                {
-                    date_channels[j] += tmp[j] + ',';
-                }
-            }
-
-            string[] names = channels_names.Split(';');
-
-            for (int i = 0; i < channel_num; i++)
-            {
-                string tm = date_channels[i];
-                string[] results = tm.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-                List<double> channel_tmp = new List<double>();
-                channel_tmp.AddRange(results.Select(x => Convert.ToDouble(x)));
-                TextBlock name = new TextBlock();
-                name.Text = names[i];
-                Grid.SetRow(name, i);
-                TestGrid.Children.Add(name);
-                CartesianChart ch = new CartesianChart();
-                ch.Series = new SeriesCollection
-                {
-                    new LineSeries
-                    {
-                        LineSmoothness = 1,
-                        StrokeThickness = 2,
-                        DataLabels = false,
-                        Fill = Brushes.Transparent,
-                        PointGeometrySize = 0,
-                        Title = names[i],
-                        Values = new ChartValues<double>(channel_tmp),
-                    }
-                };
-                ch.AxisX = new AxesCollection
-                {
-                    new Axis
-                    {
-                        ShowLabels = false,
-                    }
-                };
-                ch.AxisY = new AxesCollection
-                {
-                    new Axis
-                    {
-                        ShowLabels = false,
-                    }
-                };
-                //ch.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
-                RowDefinition rowDef = new RowDefinition();
-                TextBlock text = new TextBlock();
-                ContextMenu menu = new ContextMenu();
-                MenuItem item = new MenuItem();
-                text.ContextMenu = menu;
-                item.Header = "осцилограмма";
-                text.ContextMenu.Items.Add(item);
-                TestGrid.RowDefinitions.Add(rowDef);
-                item.Click += Open_osc;
-                Grid.SetRow(ch, i);
-
-                TestGrid.Children.Add(ch);
-
-                Grid.SetRow(text, i);
-
-                TestGrid.Children.Add(text);
-            }
+            Values = new ChartValues<double> { };
+            Values2 = new ChartValues<double> { };
+            Values3 = new ChartValues<double> { };
+            Values4 = new ChartValues<double> { };
+            Values5 = new ChartValues<double> { };
+            Values6 = new ChartValues<double> { };
+            DataContext = this;
         }
-        void Open_osc(object sender, RoutedEventArgs e)
-        {
-            {
-                Window window = new Window
-                {
-                    Title = "My User Control Dialog",
-                    Content = new OSC()
-                };
+        public ChartValues<double> Values { get; set; }
+        public ChartValues<double> Values2 { get; set; }
+        public ChartValues<double> Values3 { get; set; }
+        public ChartValues<double> Values4 { get; set; }
+        public ChartValues<double> Values5 { get; set; }
+        public ChartValues<double> Values6 { get; set; }
 
-                window.ShowDialog();
-            }
+
+        private void UpdateOnclick(object sender, RoutedEventArgs e)
+        {
+            Chart.Update(true);
+            Chart2.Update(true);
+            Chart3.Update(true);
+            Chart4.Update(true);
+            Chart5.Update(true);
+            Chart6.Update(true);
         }
     }
 }
